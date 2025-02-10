172f2b5b5ff24ee3286290bedf0993795ba036eb
FromFractalWaves <ems910@outlook.com>
2025-02-10 14:52:20 -0500

    more towards new feature!

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
-// src/store/index.ts
+
+// src/store/index.ts - Update store creation
 import { create } from 'zustand';
 import { devtools, persist } from 'zustand/middleware';
 import { StoreState } from './types';
@@ -7,30 +8,30 @@ import { createTaskSlice } from './slices/tasks';
 import { createObjectiveSlice } from './slices/objectives';
 import { createTodoListSlice } from './slices/todoLists';
 import { createTimeEntrySlice } from './slices/timeEntries';
-import { createCardSlice } from './slices/cards';
+import { createCardSlice } from './slices/card';
 
 const createStore = () => 
   create<StoreState>()(
     devtools(
       persist(
         (...a) => ({
+          ...createCardSlice(...a),
           ...createProjectSlice(...a),
           ...createTaskSlice(...a),
           ...createObjectiveSlice(...a),
           ...createTodoListSlice(...a),
           ...createTimeEntrySlice(...a),
-          ...createCardSlice(...a),
         }),
         {
           name: 'project-management-store',
           partialize: (state) => ({
+            cardView: state.cardView,
+            cardVariant: state.cardVariant,
             projects: state.projects,
             objectives: state.objectives,
             tasks: state.tasks,
             todoLists: state.todoLists,
             timeEntries: state.timeEntries,
-            cardView: state.cardView,
-            cardVariant: state.cardVariant,
           })
         }
       )
--- src/store/types.ts
+++ src/store/types.ts
@@ -1,18 +1,15 @@
 // src/store/types.ts
+import { CardSlice } from './slices/card';
 import { ProjectSlice } from './slices/project';
 import { TaskSlice } from './slices/tasks';
 import { ObjectiveSlice } from './slices/objectives';
 import { TodoListSlice } from './slices/todoLists';
 import { TimeEntrySlice } from './slices/timeEntries';
 
-export interface GlobalSlice {
-  refreshAll: () => Promise<void>;
-  clearErrors: () => void;
-}
-
-export type StoreState = ProjectSlice & 
-  TaskSlice & 
-  ObjectiveSlice & 
-  TodoListSlice & 
-  TimeEntrySlice & 
-  GlobalSlice;
+export type StoreState = 
+  & CardSlice 
+  & ProjectSlice 
+  & TaskSlice 
+  & ObjectiveSlice 
+  & TodoListSlice 
+  & TimeEntrySlice;
