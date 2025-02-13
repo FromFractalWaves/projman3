f394a1e0c9f472ecdfbce91e71d59354dda864c3
FromFractalWaves <ems910@outlook.com>
2025-02-13 11:51:38 -0500

    changed index

--- src/components/dashboard/DashboardProjects/index.ts
+++ src/components/dashboard/DashboardProjects/index.ts
@@ -1,3 +1,3 @@
 // src/components/dashboard/DashboardProjects/index.ts
-export { DashboardProjects } from './DashboardProjects';
+export * from './DashboardProjects';
 export type { DashboardProjectsProps } from '@/types/dashboard/projects';
