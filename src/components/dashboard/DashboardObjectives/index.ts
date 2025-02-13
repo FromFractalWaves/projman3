12355b08e2bbf58fa28f20b3500f6db62b5d503d
FromFractalWaves <ems910@outlook.com>
2025-02-13 11:44:56 -0500

    dont forget this

---
+++ src/components/dashboard/DashboardObjectives/index.ts
@@ -0,0 +1,3 @@
+// src/components/dashboard/DashboardObjectives/index.ts
+export { DashboardObjectives } from './DashboardObjectives';
+export type { DashboardObjectivesProps } from '@/types/dashboard/objectives';
